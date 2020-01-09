fb7ac33547f7fcd70f586fdf640cdc701d3c034e
Eric Rochester <eric.rochester@willowtreeapps.com>
2020-01-09 05:14:11 -0500

    Allocate less memory.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use env_logger;
 use log::debug;
+use std::char;
+use std::fmt::Write;
 use structopt::StructOpt;
 use rayon::prelude::*;
 use ring::digest::{Context, Digest, SHA256};
@@ -13,23 +15,42 @@ fn main() -> Result<()> {
     env_logger::init();
     let args = Cli::from_args();
 
-    debug!("Generating rainbow table with pepper value = '{}'", args.pepper);
-
+    let pepper = args.pepper.as_bytes();
     let max_id = 10u64.pow(args.digits);
-    let data = (0..max_id)
-        .map(|n| (n, format!("{:10}+{}", n, &args.pepper)))
-        .collect::<Vec<_>>();
+    let thread_count = rayon::current_num_threads();
+    let chunk_size = max_id as usize / thread_count;
+    let data: Box<[u64]> = (0..max_id).collect();
+
+    debug!("Generating rainbow table with pepper value = '{}'", args.pepper);
+    debug!("Using chunk size of {}", chunk_size);
 
     data
-        .par_iter()
-        .map(|(n, input)| {
-            let mut context = Context::new(&SHA256);
-            context.update(input.as_bytes());
-            let digest = context.finish();
-            let hash = HEXUPPER.encode(digest.as_ref());
-            (n, hash)
+        .par_chunks(chunk_size)
+        .map(|ids| {
+            let mut buffer = String::with_capacity(10);
+            let mut output = String::new();
+
+            for id in ids {
+                let mut context = Context::new(&SHA256);
+
+                buffer.clear();
+                for i in 0..11 {
+                    buffer.push(char::from_digit((id / 10u64.pow(i) % 10) as u32, 10).unwrap_or_default());
+                }
+
+                context.update(buffer.as_bytes());
+                context.update(b"+");
+                context.update(&pepper);
+
+                let digest = context.finish();
+                let hash = HEXUPPER.encode(digest.as_ref());
+
+                writeln!(&mut output, "{}\t{}", id, hash).unwrap();
+            }
+
+            output
         })
-        .for_each(|(n, hash)| println!("{}\t{}", n, hash));
+        .for_each(|output| print!("{}", output));
 
     Ok(())
 }
