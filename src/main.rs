d0d15c1b403ecd904e224a3b7e5a71c2b1726968
Eric Rochester <eric.rochester@willowtreeapps.com>
2020-01-09 10:55:50 -0500

    Process memory-friendly ranges.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use env_logger;
 use log::debug;
 use std::char;
+use std::ops::Range;
 use std::fmt::Write;
 use structopt::StructOpt;
 use rayon::prelude::*;
-use ring::digest::{Context, Digest, SHA256};
+use ring::digest::{Context, SHA256};
 use data_encoding::HEXUPPER;
 
 mod error;
@@ -17,20 +18,19 @@ fn main() -> Result<()> {
 
     let pepper = args.pepper.as_bytes();
     let max_id = 10u64.pow(args.digits);
-    let thread_count = rayon::current_num_threads();
-    let chunk_size = max_id as usize / thread_count;
-    let data: Box<[u64]> = (0..max_id).collect();
+    let thread_count = rayon::current_num_threads() as u64;
+    let mut chunks = partition_chunks(max_id, thread_count);
 
     debug!("Generating rainbow table with pepper value = '{}'", args.pepper);
-    debug!("Using chunk size of {}", chunk_size);
+    debug!("Using {} chunks of size {}", chunks.len(), chunks.get(0).map(|r| r.end - r.start).unwrap_or_default());
 
-    data
-        .par_chunks(chunk_size)
-        .map(|ids| {
+    chunks
+        .par_iter_mut()
+        .map(|chunk| {
             let mut buffer = String::with_capacity(10);
             let mut output = String::new();
 
-            for id in ids {
+            for id in chunk {
                 let mut context = Context::new(&SHA256);
 
                 buffer.clear();
@@ -45,6 +45,8 @@ fn main() -> Result<()> {
                 let digest = context.finish();
                 let hash = HEXUPPER.encode(digest.as_ref());
 
+                // TODO: Don't actually care about order, just that they don't get mixed together.
+                // Maybe create a separate thread with a channel for handling output.
                 writeln!(&mut output, "{}\t{}", id, hash).unwrap();
             }
 
@@ -66,15 +68,54 @@ struct Cli {
     pepper: String,
 }
 
-fn sha256_digest(n: u64, pepper: &str) -> Digest {
-    let mut context = Context::new(&SHA256);
-    let data = format!("{:10}+{}", n, pepper);
-    context.update(&data.as_bytes());
-    context.finish()
+/// Use a collection of ranges to implement the data to be processed broken into jobs to match
+/// the number of cores in a memory-friendly way.
+fn partition_chunks(max_value: u64, chunk_count: u64) -> Vec<Range<u64>> {
+    let mut chunks = Vec::with_capacity(chunk_count as usize);
+    let chunk_size = max_value / chunk_count;
+    let remainder = max_value % chunk_count;
+
+    for i in 0..chunk_count {
+        chunks.push((i * chunk_size) .. ((i + 1) * chunk_size));
+    }
+
+    if remainder != 0 {
+        chunks.push((chunk_count * chunk_size) .. max_value);
+    }
+
+    chunks
 }
 
-fn hash_id<R: AsRef<str>>(n: u64, pepper: R) -> (u64, String) {
-    let digest = sha256_digest(n, pepper.as_ref());
-    let hash = HEXUPPER.encode(digest.as_ref());
-    (n, hash)
+#[cfg(test)]
+mod test {
+    mod partition_chunks {
+        use super::super::*;
+        use spectral::prelude::*;
+
+        #[test]
+        fn test_partitions_evenly() {
+            let chunks = partition_chunks(12, 3);
+            assert_that(&chunks).has_length(3);
+        }
+
+        #[test]
+        fn test_partitions_returns_ranges() {
+            let chunks = partition_chunks(12, 3);
+            assert_that(&chunks[0]).is_equal_to(&(0..4));
+            assert_that(&chunks[1]).is_equal_to(&(4..8));
+            assert_that(&chunks[2]).is_equal_to(&(8..12));
+        }
+
+        #[test]
+        fn test_partitions_oddly() {
+            let chunks = partition_chunks(13, 3);
+            assert_that(&chunks).has_length(4);
+        }
+
+        #[test]
+        fn test_partition_return_right_last_range() {
+            let chunks = partition_chunks(13, 3);
+            assert_that(&chunks[3]).is_equal_to(&(12..13));
+        }
+    }
 }
